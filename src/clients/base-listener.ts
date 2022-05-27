3900e53373bc66c14af6c7626ddae541f1d07456
Shaw Lu <shawlu95@gmail.com>
2022-05-27 03:49:46 -0700

    Add NATS support

---
+++ src/clients/base-listener.ts
@@ -0,0 +1,54 @@
+import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from '../events/subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
+  abstract queueGroupName: string;
+  abstract onMessage(data: T['data'], msg: Message): void;
+
+  private client: Stan;
+
+  // subclass can define it
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDeliverAllAvailable()
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const sub = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    sub.on('message', (msg: Message) => {
+      const seq = msg.getSequence() as number;
+      console.log(`${this.subject}:${this.queueGroupName} received ${seq}`);
+
+      const parsed = this.parseMessage(msg);
+      this.onMessage(parsed, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}
